aaad83de4f09399cc1264d3c080494fe69123b89
harf1n <100131883+harf1n@users.noreply.github.com>
2023-11-24 00:26:57 +0300

    Add files via upload

---
+++ 3-SoftwareConstruction/3-Program-Сlasses-Example/SafeWaterService.java
@@ -0,0 +1,9 @@
+package com.uedge.lab9.service;
+
+import com.uedge.lab9.model.SafeWater;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SafeWaterService extends JpaRepository<SafeWater, Long> {
+
+
+}
